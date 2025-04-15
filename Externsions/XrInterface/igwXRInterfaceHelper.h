8a10205fee650e3fee90cd69f9f69f290bc1dd57
2754743118 <2754743118@qq.com>
2025-04-15 17:25:34 +0800

    add pro

---
+++ Externsions/XrInterface/igwXRInterfaceHelper.h
@@ -0,0 +1,38 @@
+﻿#ifndef IGWXRINTERFACEHELPER_H
+#define IGWXRINTERFACEHELPER_H
+
+#include "vtkNew.h"
+#include "vtkObject.h"
+#include "vtkVRCamera.h"
+#include "vtkOpenGLRenderer.h"
+class igwXRInterfaceHelper : public  vtkObject
+{
+public:
+    static igwXRInterfaceHelper* New();
+    vtkTypeMacro(igwXRInterfaceHelper, vtkObject);
+    void PrintSelf(ostream& os, vtkIndent indent) override;
+
+    void ShowXRView();
+    void SendToXR();
+
+    void RenderXRView();
+
+    void DoOneEvent();
+    void UpdateProps();
+protected:
+    igwXRInterfaceHelper();
+     ~igwXRInterfaceHelper() override;
+private:
+    igwXRInterfaceHelper(const igwXRInterfaceHelper&) = delete;
+    void operator=(const igwXRInterfaceHelper&) = delete;
+
+    vtkSmartPointer<vtkOpenGLRenderer> Renderer;
+    vtkNew<vtkPropCollection> AddedProps;
+
+    std::string RemotingAddress = "192.168.148.206";
+
+    struct igwInternals;
+    std::unique_ptr<igwInternals> Internals;
+};
+
+#endif // IGWXRINTERFACEHELPER_H
